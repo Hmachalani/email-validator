96e5f9eec1b5b34cddb639e87e7a2aeb00a8ac79
hmachalani <hmachalani@gmail.com>
2016-12-26 17:51:08 -0500

    Version 0.1
    Version 0.1

---
+++ main.js
@@ -0,0 +1,92 @@
+
+var emailExists = require("email-full-validate"),
+    csv = require("fast-csv"),
+    fs = require('fs');
+
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+
+app.post('/upload', function (req, res) {
+
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = true;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/uploads');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function (field, file) {
+        var newPath = path.join(form.uploadDir, file.name);
+        fs.rename(file.path, newPath);
+
+        validateEmails(newPath);
+
+    });
+
+    // log any errors that occur
+    form.on('error', function (err) {
+        console.log('An error has occured: \n' + err);
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function () {
+        res.end('success');
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
+
+});
+
+
+var server = app.listen(8080, function () {
+    console.log('Server listening on port 8080');
+});
+
+
+
+
+var validateEmails = function (path) {
+    var stream = fs.createReadStream(path);
+
+    var csvStream = csv()
+        .on("data", function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                validateEmail(data[i]);
+            }
+
+        })
+        .on("end", function () {
+            console.log("done");
+        });
+
+    stream.pipe(csvStream);
+
+}
+
+
+
+
+var validateEmail = function (email) {
+    emailExists.check(email, function (err, res) {
+        console.log(email +" valid? "+res);
+        if(err)console.err(err);
+    });
+}
